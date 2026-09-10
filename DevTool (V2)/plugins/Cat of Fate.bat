91e11029799aa62da4cd3b8af634d097c6ea76fa
Danya2011 <bondarecdanilo5@gmail.com>
2026-09-10 21:22:35 +0200

    Update Cat of Fate.bat

--- DevTool (V2)/plugins/Cat of Fate.bat
+++ DevTool (V2)/plugins/Cat of Fate.bat
@@ -11,10 +11,8 @@ echo.
 
 set /p user_query="Ask a cat: "
 
-:: Перевірка чи не порожнє введення
 if "%user_query%"=="" exit /b
 
-:: Генеруємо випадкове число від 1 до 3
 set /a rnd=%RANDOM% %% 3 + 1
 
 echo.
